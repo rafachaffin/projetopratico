d1b990fba65c7c503864b458ea4f6c15bb7fb99f
rafachaffin <117676850+rafachaffin@users.noreply.github.com>
2023-12-07 14:54:35 -0300

    Update Prática 9 .ino

--- Prática 9/Prática 9 .ino
+++ Prática 9/Prática 9 .ino
@@ -3,22 +3,29 @@
 
 #define IR_PIN_RECEIVER 13
 #define SERVOX_PIN 9
+#define SERVOG_PIN 8
 #define SERVOY_PIN 10
 #define SERVOZ_PIN 11
 
 Servo servoX;
 Servo servoY;
+Servo servoG;
 Servo servoZ;
 
 void moveServo(Servo &servo, int angle);
 int comandoRecebido(uint8_t comando);
 
 void setup() {
+ 
   servoX.attach(SERVOX_PIN); // inicia o servo
+  servoG.attach(SERVOG_PIN); // inicia o servo
   servoY.attach(SERVOY_PIN); // inicia o servo
   servoZ.attach(SERVOZ_PIN); // inicia o servo
   Serial.begin(9600);
- 
+   servoX.write(90);
+   servoY.write(0);
+   servoZ.write(180);
+ // Serial.println("Teste de IR no Servo:");
   IrReceiver.begin(IR_PIN_RECEIVER);
 }
 
@@ -31,37 +38,47 @@ void moveServo(Servo &servo, int angle) {
   int currentPosition = servo.read();
   int targetPosition = constrain(currentPosition + angle, 0, 180);
   servo.write(targetPosition);
-  delay(15); // Pequeno atraso para permitir que o servo atinja a posição desejada
+  delay(25);//Pequeno atraso para permitir que o servo atinja a posição desejada
 }
   
 int comandoRecebido(uint8_t comando){
   Serial.println(comando);
   IrReceiver.resume();
   switch(comando){
-      case 4:
-          	moveServo(servoX, -10);
-    		IrReceiver.resume(); 
-        	break;
-    case 6:
-          	moveServo(servoX, 10);
-    		IrReceiver.resume();
-    		break;
-    case 1:
-          	moveServo(servoY, -10);
-    		IrReceiver.resume(); 
-        	break;
-    case 9:
-          	moveServo(servoY, 10);
-    		IrReceiver.resume(); 
-        	break;
-    case 8:
-         	moveServo(servoZ, -10);
-    		IrReceiver.resume(); 
-        	break;
-    case 10:
-          	moveServo(servoZ, 10);
-    		IrReceiver.resume(); 
-        	break;
+      case 8:
+            moveServo(servoX, -10);
+        IrReceiver.resume(); 
+          break;
+    case 90:
+            moveServo(servoX, 10);
+        IrReceiver.resume();
+        break;
+    case 82:
+            moveServo(servoY, -10);
+        IrReceiver.resume(); 
+          break;
+    case 24:
+            moveServo(servoY, 10);
+        IrReceiver.resume(); 
+          break;
+    case 22:
+          moveServo(servoZ, -10);
+     //   if(servoZ.read 
+        IrReceiver.resume(); 
+          break;
+    case 13:
+            moveServo(servoZ, 10);
+        IrReceiver.resume(); 
+          break;
+    case 69:
+       
+        moveServo(servoG, -10);
+        IrReceiver.resume(); 
+          break;
+    case 71:
+            moveServo(servoG, 10);
+        IrReceiver.resume(); 
+          break;
    
 
     IrReceiver.resume(); // Reinicia o receptor IR para receber o próximo sinal
